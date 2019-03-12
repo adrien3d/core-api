6ace6b2fe60d500bbb5abd0cd36d2008e9194188
Adrien Chapelet <adrien3d@gmail.com>
2019-03-12 12:17:59 +0100

    Added user model and mongo connector

---
+++ src/models/user.model.ts
@@ -0,0 +1,67 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    id: true,
+    generated: true,
+  })
+  id: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+  @property({
+    type: 'string',
+  })
+  firstName?: string;
+
+  @property({
+    type: 'string',
+  })
+  lastName?: string;
+
+  @property({
+    type: 'string',
+  })
+  phone?: string;
+
+  @property({
+    type: 'boolean',
+  })
+  active?: boolean;
+
+  @property({
+    type: 'string',
+  })
+  activationKey?: string;
+
+  @property({
+    type: 'string',
+  })
+  resetKey?: string;
+
+  @property({
+    type: 'boolean',
+  })
+  admin?: boolean;
+
+  @property({
+    type: 'number',
+  })
+  lastAccess?: number;
+
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
